5b1daf32087057ab24cf09688744d1c8ed0d60bd
JonasFarias93 <166467757+JonasFarias93@users.noreply.github.com>
2024-09-22 12:30:33 -0300

    Criando e organizando alguns models, referente a round e arenas

--- app/tournaments/admin.py
+++ app/tournaments/admin.py
@@ -12,3 +12,23 @@ class RoundAdmin(admin.ModelAdmin):
     search_fields = ('tornament', 'date')
 
 admin.site.register(Round, RoundAdmin)
+
+class ParticipantRoundAdmin(admin.ModelAdmin):
+    list_display = ('round','participant','points','position')
+    search_fields = ('round', 'participant')
+
+admin.site.register(ParticipantRound, ParticipantRoundAdmin)
+
+class FinalRankingAdmin(admin.ModelAdmin):
+    list_display = ('tornament','participant','points','position')
+    search_fields = ('tornament', 'participant')
+
+admin.site.register(FinalRanking, FinalRankingAdmin)
+
+class NivelBlindAdmin(admin.ModelAdmin):
+    list_display = ('tornament','number','small_blind','big_blind','duration')
+    search_fields = ('tornament')
+
+class ArenaAdmin(admin.ModelAdmin):
+    list_display = ('id','name','adress','city','description')
+    search_fields = ('name','city')
--- app/tournaments/models.py
+++ app/tournaments/models.py
@@ -1,16 +1,27 @@
 from django.db import models
+from players.models import Participant
 
 
 class Tornament(models.Model):
-    ano = models.PositiveIntegerField()
+    year = models.PositiveIntegerField()
     name_tornament = models.CharField(max_length=30, blank=False, null=False, default='Nome do Torneio não Informado.')
     description = models.CharField(max_length=500, blank=True, null=True)
+
+class Arena(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, blank=False, null=False, default='Nome Não Informado')
+    adress = models.CharField(max_length=150, blank=False, null=False, default='Endereço não Informado')
+    city = models.CharField(max_length=30, blank=False, null=False, default='Cidade não Informada')
+    description = models.CharField(max_length=500, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=10, decimal_places=6, null=True)
 
 class Round(models.Model):
     tornament = models.ForeignKey(Tornament, on_delete=models.CASCADE)
     number = models.PositiveIntegerField()
     date = models.DateField()
     extra = models.BooleanField(default=False)
+    arena = models.ForeignKey(Arena, on_delete=models.PROTECT, blank=True, null=True)
 
 class ParticipantRound(models.Model):
     round = models.ForeignKey(Round, on_delete=models.CASCADE)
@@ -31,11 +42,3 @@ class NivelBlind(models.Model):
     big_blind = models.DecimalField(max_digits=10, decimal_places=2)
     duration = models.PositiveIntegerField()
 
-class Arena(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30, blank=False, null=False, default='Nome Não Informado')
-    adress = models.CharField(max_length=150, blank=False, null=False, default='Endereço não Informado')
-    city = models.CharField(max_length=30, blank=False, null=False, default='Cidade não Informada')
-    description = models.CharField(max_length=500, blank=True, null=True)
-    latitude = models.DecimalField(max_digits=10, decimal_places=6, null=True)
-    longitude = models.DecimalField(max_digits=10, decimal_places=6, null=True)
