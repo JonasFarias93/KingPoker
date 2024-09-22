4c5e9c81dafe23823f5bbfd004e91d42bb8fa2d0
JonasFarias93 <166467757+JonasFarias93@users.noreply.github.com>
2024-09-21 21:56:48 -0300

    fazendo o primeiro migrate

--- app/players/models.py
+++ app/players/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class participant(models.Model):
+class Participant(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, blank=False, null=False)
     points = models.IntegerField()
