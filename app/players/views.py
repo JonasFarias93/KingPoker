aa6db54f596489cbe8f09d620271a3142cecedbc
JonasFarias93 <166467757+JonasFarias93@users.noreply.github.com>
2024-09-22 18:05:56 -0300

    fazendo o deploy da pagina em contrução

--- app/players/views.py
+++ app/players/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def players_views(request):
-    return render(request,'players.html')
+    return render(request,'players.html',
+    {'Participant':{'name':'Jonas'}
+
+    })
 
 def index_views(request):
     return render(request, 'index.html')
