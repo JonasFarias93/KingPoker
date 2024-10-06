3ce3827141a3a91bb46c98706a98a2763dcf8baf
JonasFarias93 <166467757+JonasFarias93@users.noreply.github.com>
2024-10-06 13:58:00 -0300

    Adicionando o arquivo .env e .env-example

--- app/players/views.py
+++ app/players/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from players.models import Participant
 
 def players_views(request):
+    players = Participant.objects.all()
     return render(request,'players.html',
-    {'Participant':{'name':'Jonas'}
-
-    })
+    {'players': players}
+    )
 
 def index_views(request):
     return render(request, 'index.html')
+
